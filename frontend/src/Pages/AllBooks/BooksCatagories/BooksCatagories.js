9606d82d296bdbd8d25045a5543ea007a5d6e89b
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-03-25 15:07:53 +0600

    re-write code on some functions 23/03/23

--- frontend/src/Pages/AllBooks/BooksCatagories/BooksCatagories.js
+++ frontend/src/Pages/AllBooks/BooksCatagories/BooksCatagories.js
@@ -1,40 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import NavigationBa from "../../Shared/NavigationBa/NavigationBa";
 import SearchIcon from "@mui/icons-material/Search";
 import "./BooksCatagories.css";
 import InterestsIcon from "@mui/icons-material/Interests";
 import CreateIcon from "@mui/icons-material/Create";
 import CloseIcon from "@mui/icons-material/Close";
+import SaveIcon from "@mui/icons-material/Save";
 
 const BooksCatagories = () => {
   const [todos, setTodos] = useState([]);
-  const [category, setCategory] = useState("");
   const [subcategory, setSubcategory] = useState("");
-  const [editIndex, setEditIndex] = useState(null);
-
-  const handleCategoryChange = (event) => {
-    setCategory(event.target.value);
-  };
-
-  const handleSubcategoryChange = (event) => {
-    setSubcategory(event.target.value);
-  };
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const subcategoryRef = useRef(null);
 
   const handleAddTodo = () => {
-    if (!category) {
-      setCategory("ক্যাটাগরি লিখুন");
-      return;
-    }
-    if (editIndex === null) {
-      setTodos([...todos, { category, subcategory }]);
+    const category = selectedCategory.trim();
+    const subcategoryText = subcategory.trim();
+    if (category !== "") {
+      const todo = { category };
+      if (subcategoryText !== "") {
+        todo.subcategory = subcategoryText;
+      }
+      setTodos([...todos, todo]);
+      setSelectedCategory("");
+      setSubcategory("");
+      subcategoryRef.current.value = "";
     } else {
-      const newTodos = [...todos];
-      newTodos[editIndex] = { category, subcategory };
-      setTodos(newTodos);
-      setEditIndex(null);
+      setSelectedCategory("ক্যাটাগরি পূরণ করুন");
     }
-    setCategory("");
-    setSubcategory("");
   };
 
   const handleDeleteTodo = (index) => {
@@ -44,10 +37,40 @@ const BooksCatagories = () => {
   };
 
   const handleEditTodo = (index, category, subcategory) => {
-    const newTodos = [...todos];
-    newTodos[index] = { category, subcategory };
-    setTodos(newTodos);
+    const todo = todos[index];
+    setSelectedCategory(todo.category);
+    setSubcategory(todo.subcategory || "");
   };
+
+  const handleSaveTodo = (index) => {
+    const category = selectedCategory.trim();
+    const subcategoryText = subcategory.trim();
+    if (category !== "") {
+      const newTodos = [...todos];
+      const todo = newTodos[index];
+      todo.category = category;
+      if (subcategoryText !== "") {
+        todo.subcategory = subcategoryText;
+      } else {
+        delete todo.subcategory;
+      }
+      setTodos(newTodos);
+      setSelectedCategory("");
+      setSubcategory("");
+      subcategoryRef.current.value = "";
+    } else {
+      setSelectedCategory("ক্যাটাগরি পূরণ করুন");
+    }
+  };
+
+  const categories = [
+    "Work",
+    "Personal",
+    "Shopping",
+    "Errands",
+    "Chores",
+    "Other",
+  ];
 
   return (
     <div>
@@ -74,17 +97,22 @@ const BooksCatagories = () => {
                 <input
                   className="catogories-input"
                   type="text"
-                  value={category}
-                  onChange={handleCategoryChange}
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
                 />
                 <select
                   className="form-select catalogue-selection-button"
                   aria-label="Default select example"
                 >
-                  <option selected>ক্যাটাগরি</option>
+                  {categories.map((category, index) => (
+                    <option key={index} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                  {/* <option selected>ক্যাটাগরি</option>
                   <option value="1">ক্যাটাগরি- ১</option>
                   <option value="2">ক্যাটাগরি- ২</option>
-                  <option value="3">ক্যাটাগরি- ৩</option>
+                  <option value="3">ক্যাটাগরি- ৩</option> */}
                 </select>
               </div>
             </div>
@@ -94,26 +122,15 @@ const BooksCatagories = () => {
                 className="sub-catogories-input"
                 type="text"
                 value={subcategory}
-                onChange={handleSubcategoryChange}
+                onChange={(e) => setSubcategory(e.target.value)}
               />
             </div>
             <button onClick={handleAddTodo} className="songrokkhon-button">
-              {editIndex !== null ? "Save" : "সংরক্ষন করুন"}
+              সংরক্ষন করুন
             </button>
           </div>
           <div className="col-xl-4 col-lg-5 col-md-5 col-sm-12 col-12">
             <div>
-              {/* {items.map((elem, ind) => {
-                return (
-                  <div className="catagories-topics" key={ind}>
-                    <InterestsIcon />
-                    <p>{elem}</p>
-                    <CloseIcon onClick={() => deleteItem(ind)} />
-                    <CreateIcon />
-                  </div>
-                );
-              })} */}
-
               {todos.length === 0 ? (
                 <p>ক্যাটাগরি / সাব-ক্যাটাগরি যোগ করুন</p>
               ) : (
@@ -124,8 +141,12 @@ const BooksCatagories = () => {
                         <InterestsIcon />
                         <span>{todo.category}</span>
                         <span>{todo.subcategory}</span>
+                        {todo.subcategory && <span> - {todo.subcategory}</span>}
                         <CreateIcon onClick={() => handleEditTodo(index)} />
                         <CloseIcon onClick={() => handleDeleteTodo(index)} />
+                        {selectedCategory !== "" && (
+                          <SaveIcon onClick={() => handleSaveTodo(index)} />
+                        )}
                       </div>
                     </li>
                   ))}
@@ -146,55 +167,6 @@ const BooksCatagories = () => {
           </div>
         </div>
       </section>
-
-      {/* <section>
-        <div>
-          <h1>Todo List</h1>
-          <div>
-            <label>
-              Category:
-              <input
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-              />
-            </label>
-            <button onClick={handleAddTodo}>Add Todo</button>
-          </div>
-          {todos.length === 0 ? (
-            <p>Please add a todo.</p>
-          ) : (
-            <ul>
-              {todos.map((todo, index) => (
-                <li key={index}>
-                  <div className="catagories-topics">
-                    <span>{todo.category}</span>
-                    {todo.subcategory ? (
-                      <span> - {todo.subcategory}</span>
-                    ) : (
-                      <button onClick={() => handleAddSubcategory(index, "")}>
-                        Add Subcategory
-                      </button>
-                    )}
-                  </div>
-                  <div>
-                    <CreateIcon
-                      onClick={() =>
-                        handleEditTodo(
-                          index,
-                          prompt("Edit Category", todo.category),
-                          prompt("Edit Subcategory", todo.subcategory)
-                        )
-                      }
-                    />
-                    <CloseIcon onClick={() => handleDeleteTodo(index)} />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </section> */}
 
       <section></section>
     </div>
