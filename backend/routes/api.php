f59af577dbd5043cff4eb1ebd8cc0c3830969d57
Md. Rakib Hossain <rakib.ba@pakiza.com>
2023-04-13 11:42:08 +0600

    updated

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\NewsCategoryController;
 
 /*
 |--------------------------------------------------------------------------
