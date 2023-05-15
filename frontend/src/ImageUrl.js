6047b0fd0c90541b34102e2c33d1d6404511282c
Md. Rakib Hossain <rakib.ba@pakiza.com>
2023-05-15 18:33:27 +0600

    updated

--- frontend/src/ImageUrl.js
+++ frontend/src/ImageUrl.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 global.imageURL = "http://172.31.120.39:8000";
 //  global.imageURL ='https://test.austtaa.com/server/public';
 // global.imageURL = "http://172.31.120.99:8000";
-=======
-// global.imageURL = "http://172.31.120.39:8000";
-//  global.imageURL ='https://test.austtaa.com/server/public';
-global.imageURL = "http://172.31.120.99:8000";
->>>>>>> 61ae0a75ddf91f265b075d6405400b7f428911f0
 // global.imageURL = "http://127.0.0.1:8000";
