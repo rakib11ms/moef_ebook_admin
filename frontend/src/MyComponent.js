ab6b00be2038ddb1cc66fb5f26963ae58816f987
Md. Rakib Hossain <rakib.ba@pakiza.com>
2023-05-17 18:17:03 +0600

    updated

--- frontend/src/MyComponent.js
+++ frontend/src/MyComponent.js
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-// const socket = io.connect('https://chat.austtaa.com/'); 
-const socket = io.connect('http://localhost:4000/'); 
+// import React, { useEffect, useState } from 'react';
+// import io from 'socket.io-client';
+// // const socket = io.connect('https://chat.austtaa.com/'); 
+// const socket = io.connect('http://localhost:4000/'); 
 
-const MyComponent = () => {
-  const [message, setMessage] = useState('');
+// const MyComponent = () => {
+//   const [message, setMessage] = useState('');
 
-  const [receivedMessage, setReceivedMessage] = useState('');
+//   const [receivedMessage, setReceivedMessage] = useState('');
 
 
-  console.log('status',socket)
+//   console.log('status',socket)
+// // 
+//   useEffect(() => {
 
-  useEffect(() => {
+//     socket.on('connect', () => {
+//       console.log('Connected to server');
+//     });
 
-    socket.on('connect', () => {
-      console.log('Connected to server');
-    });
+//     socket.on('message', (data) => {
+//       console.log('Received message:', data);
+//       setReceivedMessage(data);
+//     });
 
-    socket.on('message', (data) => {
-      console.log('Received message:', data);
-      setReceivedMessage(data);
-    });
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, [socket]);
 
-    return () => {
-      socket.disconnect();
-    };
-  }, [socket]);
+//   const sendMessage = () => {
+//     socket.emit('message', message);
+//     setMessage('');
+//   };
 
-  const sendMessage = () => {
-    socket.emit('message', message);
-    setMessage('');
-  };
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//       />
+//       <button onClick={sendMessage}>Send Message</button>
+//       <p>Received Message: {receivedMessage}</p>
+//     </div>
+//   );
+// };
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send Message</button>
-      <p>Received Message: {receivedMessage}</p>
-    </div>
-  );
-};
-
-export default MyComponent;
+// export default MyComponent;
