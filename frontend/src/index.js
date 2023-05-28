58694bb0d991a8856a779a19c495e920b0cf6ee9
Md. Rakib Hossain <rakib.ba@pakiza.com>
2023-05-28 18:22:08 +0600

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
 
+
 axios.interceptors.request.use(
   function (config) {
     const token = localStorage.getItem("auth_token");
